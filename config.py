a9e4388afb145916fd69db100a55f67c6dc874fd
mSujal <pandit.sujal2061@gmail.com>
2026-01-04 23:58:25 +0545

    added configuration constants

---
+++ config.py
@@ -0,0 +1,20 @@
+####################################################
+#    PANEL PARAMETERS
+####################################################
+
+# sidebars (left and right)
+SIDEBAR_BG= '#f0f0f0'
+SIDEBAR_BORDER_RIGHT= '1px solid #ccc' # immediate value, could be broken or changed at once
+SIDEBAR_BORDER_LEFT = SIDEBAR_BORDER_RIGHT
+SIDEBAR_TITLE_FONT_SIZE= '16px'
+SIDEBAR_TITLE_FONT_WEIGHT= 'bold'
+SIDEBAR_TITLE_PADDING= '10px'
+SIDEBAR_PADDING= '10px'
+SIDEBAR_MIN_WIDTH= 200
+SIDEBAR_MAX_WIDTH= 300
+SIDEBAR_FONT_SIZE= '14px'
+SIDEBAR_FONT_PADDING= '20px'
+
+# Pdf area
+CENTER_BG= 'white' # placeholder, need to change 
+
